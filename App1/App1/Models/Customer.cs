089ad0ca1dcb0495c4e0fa1524b4758859d60f48
Alexander Guan <alexander.guan.sp3@outlook.com>
2015-07-12 19:26:21 +0800

    Chapter 3
    Chapter 3

---
+++ App1/App1/Models/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App1.Models
+{
+    public class Customer: INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return this._firstName; }
+            set
+            {
+                if(value != _firstName)
+                {
+                    this._firstName = value;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName 
+        {
+            get { return this._lastName; } 
+            set
+            {
+                if(value != _lastName)
+                {
+                    this._lastName = value;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged([CallerMemberName]string member = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(member));
+        }
+    }
+}
---
+++ App1/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Models;
+using App1.Common;
+
+namespace App1.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public Customer MyCustomer { get; set; }
+
+        public MainPageViewModel()
+        {
+            MyCustomer = new Customer { FirstName = "Bob", LastName = "Smith" };
+        }
+
+        private RelayCommand _updateNameCommand;
+
+        private void UpdateName()
+        {
+            MyCustomer.FirstName = "Sue";
+        }
+
+        public RelayCommand UpdateNameCommand
+        {
+            get
+            {
+                if (_updateNameCommand == null) _updateNameCommand = new RelayCommand(UpdateName);
+                return this._updateNameCommand;
+            }
+            set
+            {
+                this._updateNameCommand = value;
+            }
+        }
+
+    }
+}
